04ab088e23d919084e510bd9dc08b9b0d036db5d
dsvaishnavi <vaishnavidhawade1@gmail.com>
2025-04-17 22:05:50 +0530

    video

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState, useEffect } from 'react';
-import Header from './components/Header';
-import Home from './sections/Home';
-import Education from './sections/Education';
-import Skills from './sections/Skills';
-import Projects from './sections/Projects';
-import Footer from './components/Footer';
-import About from './sections/About';
-import ThemeToggle from './components/ThemeToggle';
-import './App.css';   
-import React from 'react';
-import videobg from './assets/videobg.mp4';
+import { useState, useEffect } from "react";
+import Header from "./components/Header";
+import Home from "./sections/Home";
+import Education from "./sections/Education";
+import Skills from "./sections/Skills";
+import Projects from "./sections/Projects";
+import Footer from "./components/Footer";
+import About from "./sections/About";
+import ThemeToggle from "./components/ThemeToggle";
+import "./App.css";
+import React from "react";
+import videobg from "./assets/videobg.mp4";
 
 function App() {
-  const [theme, setTheme] = useState('dark');
+  const [theme, setTheme] = useState("dark");
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem('theme') || 'dark';
+    const savedTheme = localStorage.getItem("theme") || "dark";
     setTheme(savedTheme);
-    document.documentElement.setAttribute('data-theme', savedTheme);
+    document.documentElement.setAttribute("data-theme", savedTheme);
   }, []);
 
   return (
-    
     <div className="App">
       <div className="videog">
-      <video src="/videobg.mp4" autoPlay muted loop />
-
-    </div>
+        <video src={videobg} autoPlay muted loop />
+      </div>
       {/* Main Content */}
       <Header />
-      <main>        
+      <main>
         <ThemeToggle />
         <Home />
         <About />
@@ -41,6 +39,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
